810588b8e155c7c5abfd2bd2b318623116e5cb5c
lewisseo91 <togokr@naver.com>
2023-03-08 09:16:57 +0900

    feat(test): 포스트 서비스 datajpatest 추가
    - soft Delete로 설정

--- assistant-post/src/main/java/com/assistant/domain/Post.java
+++ assistant-post/src/main/java/com/assistant/domain/Post.java
@@ -1,9 +1,30 @@
 package com.assistant.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private final Long postId;
+
+    @Column(name = "author_id")
     private final Long authorId;
+
+    @Column(name = "title")
     private final String title;
+
+    public Post() {
+        postId = null;
+        authorId = null;
+        title = null;
+    }
 
     public Post(Long postId, Long authorId, String title) {
         this.postId = postId;
