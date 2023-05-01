94c4a04b1028d940320254aad3f742fd9b671e4b
lewisseo91 <togokr@naver.com>
2023-05-02 00:40:06 +0900

    test(shape) : shape 생성 테스트 추가

---
+++ assistant-post/src/test/java/com/assistant/service/ShapeServiceTest.java
@@ -0,0 +1,41 @@
+package com.assistant.service;
+
+import com.assistant.domain.Shape;
+import com.assistant.domain.ShapeStyle;
+import com.assistant.dto.CanvasCreateRequest;
+import com.assistant.dto.PointCreateRequest;
+import com.assistant.dto.ShapeCreateRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@DataJpaTest
+@Import(ShapeService.class)
+class ShapeServiceTest {
+    @Autowired
+    private ShapeService shapeService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void 쉐입은_생성된다() {
+        Long shapeId = 1L;
+        Long canvasId = 1L;
+        String orientation = "portrait";
+        String fontStyle = "italic";
+        List<PointCreateRequest> pointCreateRequests = List.of();
+        ShapeCreateRequest shapeCreateRequest = new ShapeCreateRequest(shapeId, canvasId, orientation, fontStyle, pointCreateRequests);
+        Shape createdShape = shapeService.create(shapeCreateRequest);
+
+        assertEquals(createdShape.getShapeId(), shapeId);
+    }
+}
